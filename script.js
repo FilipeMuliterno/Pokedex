e5d68a57314840f58d1990fbdee262c112e5a718
Filipe Muliterno <filipemuliternop@hotmail.com>
2023-04-17 21:59:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var quantidade = document.getElementById('quantidade');
+quantidade.addEventListener('keyup',()=>{
+    pegaPokemons(quantidade.value);
+})
+function pegaPokemons(quantidade){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10'+quantidade)
+    .then(Response => Response.json())
+    .then(allpokemon => {
+
+        var pokemons = [];
+
+        allpokemon.results.map((val)=>{
+            
+
+            fetch(val.url)
+            .then(Response => Response.json())
+            .then(pokemonSingle => {
+                pokemons.push({nome:val.name,imagem:pokemonSingle.sprites.front_default});
+                
+                if(pokemons.length == quantidade){
+                    //Finalizamos nossas requisições.
+                    
+                    var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                    pokemonBoxes.innerHTML = "";
+
+                    pokemons.map(function(val){
+                        pokemonBoxes.innerHTML+=`
+                        <div class="pokemon-box">
+                        <img src="`+val.imagem+`">
+                        <p>`+val.nome+`</p>
+                        </div>
+                        `;                       
+                    })
+                }          
+            })
+        })
+    })
+}
